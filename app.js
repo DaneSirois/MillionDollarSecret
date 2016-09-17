ec95cb6beb4e0bedbe9ec94dbbd71a7a57602e91
Dane Sirois <dane_sirois@live.com>
2016-09-16 23:28:54 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var myip = require('quick-local-ip');
+const publicIp = require('public-ip');
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+var server = require('http').createServer(app);
+
+app.set('port', (process.env.PORT || 8080));
+
+/* Get users IP and send to client */
+app.use(function(req, res, next) {
+  publicIp.v4().then(ip => {
+    console.log(ip);
+    //=> '46.5.21.123'
+  });
+ // var clientIP = myip.getLocalIP4();
+
+    next();
+});
+
+app.use(express.static(path.join(__dirname, '/client')));       // set the static files location
+app.use(logger('dev'));                         // log every request to the console
+app.use(bodyParser());                          // pull information from html in POST
+app.use(methodOverride());                      // simulate DELETE and PUT
+app.use(favicon(__dirname + '/client/css/img/favicon.ico'));
+
+
+app.use('/', routes);
+app.use('/thesecret', routes);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+server.listen(app.get('port'));
+
+module.exports = app;
---
+++ client/untitled.js
@@ -0,0 +1,22 @@
+let createUser = () => {
+  /
+}
+
+let checkDatabaseForMatchingIp = (ipAddress) => {
+
+};
+
+// when user navigates to '/thesecret'
+let checkIfReturningUser = (ipAddress) => {
+  if (checkDatabaseForMatchingIp(ipAddress) === false) {
+    //deny access
+    //redirect to '/'
+  } else {
+   //Grant access
+  }
+};
+
+{
+  "user": ip....,
+  "paymentComplete": false
+}
